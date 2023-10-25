e11a9767d04a9b954781e5712875f963b50be9e3
Jeriel <jerielcastro87@gmail.com>
2023-10-25 13:36:40 +0800

    Revert "Merge branch 'main' into jeriel-customerpages"
    This reverts commit 885b8f469344e1da92da757447e575441e8de790, reversing
    changes made to 0eb8a6473ac98e95ad17c64eb9bf339cfdbcac4f.

--- Werkzeug.sh
+++
@@ -1,3 +0,0 @@
-#can't place this in requirements.txt, else have error
-#!/bin/bash
-pip install --force-reinstall Werkzeug==2.3.0
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from website import create_app
-#ta
+
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
     app.run(debug=True)
