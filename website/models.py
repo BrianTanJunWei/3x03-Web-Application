96205261bc0b02982623a72a5c1ecc8b6a7cfeba
Jeriel <jerielcastro87@gmail.com>
2023-10-25 22:42:09 +0800

    Update models.py

--- website/models.py
+++ website/models.py
@@ -25,7 +25,6 @@ class Product(db.Model):
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
     price = db.Column(db.Float, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     image = db.Column(db.String(255), nullable=True)
     users = db.relationship("User", back_populates="cart")
     cart_items = db.relationship("CartItem", back_populates="product")
