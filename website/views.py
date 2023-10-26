2f97485970e576dbb811f9dd07dd4f275acf4386
Jeriel <jerielcastro87@gmail.com>
2023-10-26 15:46:45 +0800

    updated for generating pdf
    pdf will be in new windows tabs

--- website/views.py
+++ website/views.py
@@ -7,6 +7,9 @@ from . import db
 from io import BytesIO
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
 
 views = Blueprint('views', __name__)
 
@@ -110,27 +113,53 @@ def mark_order_as_paid(user, total_cost):
 
 def generate_order_pdf(user, cart_items, total_cost):
     buffer = BytesIO()
-    c = canvas.Canvas(buffer, pagesize=letter)
+    doc = SimpleDocTemplate(buffer, pagesize=letter)
 
-    # Customize the PDF layout and content
-    c.setFont("Helvetica", 12)
-    c.drawString(100, 750, "Order Summary")
-    c.drawString(100, 730, f"User: {user.first_name} {user.last_name}")
+    # Create a list to hold PDF elements
+    elements = []
 
-    # Iterate through cart items
-    y = 710  # Starting y-coordinate
+    # Styles for the PDF
+    styles = getSampleStyleSheet()
+
+    # Title
+    title = Paragraph("Order Summary", styles['Title'])
+    elements.append(title)
+
+    # Date
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    date_text = f"Order Date: {current_date}"
+    elements.append(Paragraph(date_text, styles['Normal']))
+    
+    # User information
+    user_info = f"User: {user.first_name} {user.last_name}"
+    if user.address:
+        user_info += f"<br />Address: {user.address}"
+    elements.append(Paragraph(user_info, styles['Normal']))
+    elements.append(Spacer(1, 12))
+
+    # Table to display order details
+    order_data = [["Product", "Quantity", "Price", "Subtotal"]]
     for cart_item in cart_items:
         product = cart_item.product
-        c.drawString(100, y, f"Product: {product.name}")
-        c.drawString(250, y, f"Quantity: {cart_item.quantity}")
-        c.drawString(350, y, f"Price: ${product.price:.2f}")
-        c.drawString(450, y, f"Subtotal: ${cart_item.quantity * product.price:.2f}")
-        y -= 20  
+        order_data.append([product.name, cart_item.quantity, f"${product.price:.2f}", f"${cart_item.quantity * product.price:.2f}"])
+    order_table = Table(order_data)
+    order_table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+    elements.append(order_table)
+    elements.append(Spacer(1, 12))
 
-    c.drawString(100, y, f"Total Cost: ${total_cost:.2f}")
+    # Total cost
+    total_cost_text = f"Total Cost: ${total_cost:.2f}"
+    elements.append(Paragraph(total_cost_text, styles['Normal']))
 
-    c.showPage()
-    c.save()
+    doc.build(elements)
 
     pdf_data = buffer.getvalue()
     buffer.close()
