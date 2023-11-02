44e15bb6594d0fd5f3484f0ffd866c193424f15d
BrianTanJunWei <91254516+BrianTanJunWei@users.noreply.github.com>
2023-11-03 00:35:04 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
+from . import db
 from .models import Login,UserAccounts,StaffAccounts
 from .models import *
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
