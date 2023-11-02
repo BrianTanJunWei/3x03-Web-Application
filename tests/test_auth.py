020b6f9a4ba2980a2dffd0fe887261a17636b91b
Jeriel <jerielcastro87@gmail.com>
2023-11-02 17:12:25 +0800

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -24,15 +24,15 @@ class TestAuth(TestCase):
     def _create_test_user(self):
         # Create a test user for login and signup tests
         hashed_password = bcrypt.generate_password_hash('password').decode('utf-8')
-        user_login = Login(email_address='testuser@example.com', password=hashed_password, account_status=True, account_type=2)
-        user_accounts = UserAccounts(email_address='testuser@example.com', first_name='Test', last_name='User')
+        user_login = Login(email_address='testuser1@example.com', password=hashed_password, account_status=True, account_type=2)
+        user_accounts = UserAccounts(email_address='testuser1@example.com', first_name='Test', last_name='User')
         db.session.add(user_login)
         db.session.add(user_accounts)
         db.session.commit()
 
     def test_login(self):
-        response = self.client.post('/login', data=dict(
-            email='testuser@example.com',
+        response = self.client.post('/login', data=dict(s
+            email='testuser1@example.com',
             password='password'
         ), follow_redirects=True)
 
@@ -43,7 +43,7 @@ class TestAuth(TestCase):
         
     def test_login_invalid_credentials(self):
         response = self.client.post('/login', data=dict(
-            email='testuser@example.com',
+            email='testuser1@example.com',
             password='wrong_password'
         ), follow_redirects=True)
 
