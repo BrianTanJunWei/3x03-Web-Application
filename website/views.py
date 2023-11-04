01099e3eaac659e9452420db5738b08502939ce9
Jeriel <jerielcastro87@gmail.com>
2023-11-04 16:48:01 +0800

    Revert "Update views.py"
    This reverts commit c3cbead832d722695bd0be1e9cc6375ddcd05c09.

--- website/views.py
+++ website/views.py
@@ -550,11 +550,11 @@ def request_password_reset():
 
     if request.method == 'POST':
         email = request.form.get('email')
-        user = Login.query.filter_by(email_address=email).first()
+        user = UserAccounts.query.filter_by(email_address=email).first()
 
         if user:
             # Create a password reset token and save it in the database
-            token = PasswordResetToken(user_id=user.id)
+            token = PasswordResetToken(user_id=user.email_address)
             db.session.add(token)
             db.session.commit()
 
@@ -569,10 +569,9 @@ def request_password_reset():
     return render_template('request_password_reset.html')
 
 def send_password_reset_email(user, token):
-    user_details = UserAccounts.query.filter(user.id).first()
     # Create the email content
     email_content = f"""
-    <p>Hello {user_details.first_name},</p>
+    <p>Hello {user.first_name},</p>
     <p>You recently requested to reset your password. Click the link below to reset your password:</p>
     <p><a href="{request.host_url}reset_password/{token}">Reset Password</a></p>
     <p>If you didn't make this request, you can ignore this email.</p>
@@ -585,7 +584,7 @@ def send_password_reset_email(user, token):
         'Content-Type': 'application/json',
     }
     data = {
-        'to': [{'email': user_details.email_address}],
+        'to': [{'email': user.email_address}],
         'subject': 'Password Reset Request',
         'htmlContent': email_content,
         'sender': {'email': SENDER_EMAIL},
