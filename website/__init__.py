a1741f1b90ae020e5caacec74bc0a5dc06564778
Marcus Ho <lvlarcusho@gmail.com>
2023-11-05 17:11:49 +0800

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -58,6 +58,7 @@ def create_app():
     def apply_csp(response):
         csp = ("default-src 'self'; "
                "script-src 'self' "
+               "'sha256-F9sK+B8DQLklcA7ODBaGvWpvpj+6mv2UNA33p3J2D88=' "
                "'sha256-m+pWPJK/WXg1QY7IloTEfEeSdGqc7WcI7GfMyxFyXvc=' "
                "'sha256-l7zFrRVV2XIn0Noa9R/8RUgL5aErbMiOYycrLARV2WI=' "
                "'sha256-YazMb6d9LlJhtWUCUEqhr5nGeGBn8/UgnFqfXKjMvEU=' "
